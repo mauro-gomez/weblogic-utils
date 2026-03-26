991f0b6baf8cfcc26e19f5aa410423f054ec2155
Mauricio Gomez <mauricio.gomez@gmail.com>
2026-03-26 20:37:46 +0000

    Fixed middleware home location

--- wlst/domain_operations/run.sh
+++ wlst/domain_operations/run.sh
@@ -3,13 +3,5 @@
 # get password from user
 echo "Enter the password for weblogic user: "
 read -s PASSWD
-# 12.2.1.4
-#/home/opc/java/Oracle_Home12214/oracle_common/common/bin/wlst.sh domain_ops_12.py --admin_url t3://localhost:7001 --username weblogic --password $PASSWD --get_thread_dumps --interactive
-# 14.1.1
-#/home/opc/java/Oracle_Home14/oracle_common/common/bin/wlst.sh domain_ops.py --admin_url t3://localhost:7001 --username weblogic --password $PASSWD --get_thread_dumps --interactive
-# 14.1.2
-#/home/opc/java/Oracle_Home1412/oracle_common/common/bin/wlst.sh domain_ops.py --admin_url t3://localhost:7001 --username weblogic --password $PASSWD --get_thread_dumps --interactive
-# 15.1.1
-#/home/opc/java/Oracle_Home1511/oracle_common/common/bin/wlst.sh domain_ops.py --admin_url t3://localhost:7001 --username weblogic --password $PASSWD --get_thread_dumps --interactive
-# 14.1.1 Quick
-/home/opc/java/Oracle_Home1411_Quick/wls1411/oracle_common/common/bin/wlst.sh domain_ops.py --admin_url t3://localhost:7001 --username weblogic --password $PASSWD --get_thread_dumps --interactive
+
+/u01/app/oracle/middleware/Oracle_Home1411_Lite/wls1411/oracle_common/common/bin/wlst.sh domain_ops.py --admin_url t3://localhost:7001 --username weblogic --password $PASSWD --get_thread_dumps --interactive
--- wlst/domain_operations/run_with_thread_dumps.sh
+++ wlst/domain_operations/run_with_thread_dumps.sh
@@ -4,4 +4,4 @@
 echo "Enter the password for weblogic user: "
 read -s PASSWD
 
-/home/opc/java/Oracle_Home14/oracle_common/common/bin/wlst.sh domain_ops.py --admin_url t3://localhost:7001 --username weblogic --password $PASSWD --get_thread_dumps
+/u01/app/oracle/middleware/Oracle_Home1411_Lite/wls1411/oracle_common/common/bin/wlst.sh domain_ops.py --admin_url t3://localhost:7001 --username weblogic --password $PASSWD --get_thread_dumps
